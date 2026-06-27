0ff0336b83e98e9ac12326c678b35e6d670afa75
William Davi Georg <william01.georg01@gmail.com>
2026-06-26 21:21:30 -0300

    feat: adiciona atividade dois da aula 6

---
+++ aula-07/pratica01_alunos.js
@@ -0,0 +1,14 @@
+let alunos = [
+    {nome: "Libertô", idade: 69, curso: "Técnico em Liberdade"},
+    {nome: "Priscila", idade: 96, curso: "Técnico em Aprisionamento"},
+    {nome: "Rebeca", idade: 67, curso: "Técnico em Rebelião"}
+]
+
+console.log(" ")
+console.log("==== lISTA COM FOR ====")
+for (let i = 0; i < alunos.length; i++) {
+    console.log(`Aluno: ${alunos[i].nome} | Idade: ${alunos[i].idade}`)
+}
+
+console.log(" ")
+console.log("==== LISTA COM FOREACH ====")
